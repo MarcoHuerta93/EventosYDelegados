8d0d2a6a27298db417d5d6bfdfd642366e20f0ae
Marco <marco.huerta.sc@pm.me>
2021-08-24 13:31:04 -0500

    Creación del Delegado y del Evento ejemplo

--- EventosYDelegados/AsistenteDescarga.cs
+++ EventosYDelegados/AsistenteDescarga.cs
@@ -5,8 +5,20 @@ namespace EventosYDelegados
 {
   public class AsistenteDescarga
     {
+        //Paso 1 crear el delegado
         public delegate void GestorEventoArchivoDescargado(object fuente, EventArgs args);
 
+        //Paso 2 Crear el evento basado en el delegado
+        public event GestorEventoArchivoDescargado ArchivoDescargado;
+
+        //Paso 3 Iniciar el evento
+
+        protected virtual void EnArchivoDescargado()
+        {
+            if (ArchivoDescargado != null) {
+                ArchivoDescargado(this, EventArgs.Empty);
+            }
+        }
 
 
         public void Descarga(Archivo archivo)
@@ -14,6 +26,8 @@ namespace EventosYDelegados
             Console.WriteLine("Descargando Archivo...");
             Thread.Sleep(4000);
 
+            //Paso 3.1
+            EnArchivoDescargado();
         }
        
     }
--- EventosYDelegados/Program.cs
+++ EventosYDelegados/Program.cs
@@ -8,8 +8,21 @@ namespace EventosYDelegados
         {
             var archivo = new Archivo() { Titulo = "Archivo 1"};
             var asistenteDescarga = new AsistenteDescarga(); //emisor
+            var servicioDesempacar = new ServicioDesempacar();//receptor
+
+            asistenteDescarga.ArchivoDescargado += servicioDesempacar.EnArchivoDescargado;
 
             asistenteDescarga.Descarga(archivo);
+            Console.ReadKey();
+        }
+    }
+
+    public class ServicioDesempacar
+    {
+        public void EnArchivoDescargado(object fuente, EventArgs e)
+        {
+            Console.WriteLine("ServicioDesempacar: desempacando el archivo...");
+
         }
     }
 }
